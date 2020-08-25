e000a46d8551acc48161f7a28e895eafd5ccf7d3
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-08-25 12:42:26 +0300

    Add task 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,1 +1,22 @@
-"use strict";
+'use strict';
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const createItemList = ingredient => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  return item;
+};
+
+const listOfIngredients = ingredients.map(ingredient =>
+  createItemList(ingredient),
+);
+
+const list = document.querySelector('ul#ingredients');
+list.append(...listOfIngredients);
