657bca0af37a6eb6668916c2f223eef9f9204782
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-08-25 15:42:50 +0300

    Add task 04 fnd 05

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,24 @@
-"use strict";
+'use strict';
+
+let counterValue = 0;
+
+const incrementButton = document.querySelector(
+  `button[data-action = "increment"]`,
+);
+const decrementButton = document.querySelector(
+  `button[data-action = "decrement"]`,
+);
+
+const clickAcc = document.querySelector('div#counter span#value');
+
+const increment = () => {
+  counterValue += 1;
+  clickAcc.textContent = counterValue;
+};
+const decrement = () => {
+  counterValue -= 1;
+  clickAcc.textContent = counterValue;
+};
+
+incrementButton.addEventListener('click', increment);
+decrementButton.addEventListener('click', decrement);
--- js/task-05.js
+++ js/task-05.js
@@ -1,1 +1,9 @@
-"use strict";
+'use strict';
+const inputRef = document.querySelector('input#name-input');
+const outputRef = document.querySelector('span#name-output');
+
+inputRef.addEventListener('input', getInputValue);
+
+function getInputValue() {
+  outputRef.textContent = inputRef.value === '' ? 'незнакомец' : inputRef.value;
+}
