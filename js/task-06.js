3788890a7232a6debca6fac258b7df7f3456ae04
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-09-05 20:26:21 +0300

    Remove task8 and rewrite code in task06

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,12 @@
-'use strict';
+
 const inputRef = document.querySelector('input#validation-input');
+
 inputRef.addEventListener('blur', validation);
+// console.log(inputRef.getAttribute("data-length"))
 
 function validation() {
-  if (inputRef.value.length === 6) {
+  
+  if (inputRef.value.length === +inputRef.getAttribute("data-length")) {
     inputRef.classList.add('valid');
     inputRef.classList.remove('invalid');
   } else {
