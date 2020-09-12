2d9067ec4560001a338154d154d9461c19e9542c
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-09-12 10:47:30 +0300

    in task 06 - change to dataser.length

--- js/task-06.js
+++ js/task-06.js
@@ -2,11 +2,10 @@
 const inputRef = document.querySelector('input#validation-input');
 
 inputRef.addEventListener('blur', validation);
-// console.log(inputRef.getAttribute("data-length"))
 
 function validation() {
   
-  if (inputRef.value.length === +inputRef.getAttribute("data-length")) {
+  if (inputRef.value.length === +inputRef.dataset.length) {
     inputRef.classList.add('valid');
     inputRef.classList.remove('invalid');
   } else {
